2801414357e55f1954812d4e4ee5d059edac9003
Moon Walker <almodook@freemail.hu>
2014-02-02 18:06:05 +0100

    h

---
+++ kkkkklk/kkkkklk.ino
@@ -0,0 +1,16 @@
+
+void setup()
+{
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(2, OUTPUT);
+
+analogWrite(5, 155);
+//digitalWrite(5,LOW);
+digitalWrite(2, LOW);
+digitalWrite(6, HIGH);
+}
+void loop()
+{
+
+}
